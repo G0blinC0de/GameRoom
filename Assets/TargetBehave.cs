f9e240961d87fe0bf5b9c0001b93caec43de7864
G0blinC0de <brewbrotherred@gmail.com>
2024-01-24 10:49:18 -0800

    add target spawn delay, target movement behavior

--- Assets/TargetBehave.cs
+++ Assets/TargetBehave.cs
@@ -1,21 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class TargetBehave : MonoBehaviour
 {
-    public float speed = 1f;
-    private Vector3 _targetPosition;
+    public float targetSpeed = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        _targetPosition = transform.position;    
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = transform.position + (Vector3.up * targetSpeed) * Time.deltaTime;
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
     }
 }
--- Assets/TargetSpawner.cs
+++ Assets/TargetSpawner.cs
@@ -3,28 +3,35 @@ using UnityEngine;
 
 public class TargetSpawner : MonoBehaviour
 {
-    private Vector3 originPosition;
     public GameObject TargetObj;
-    public float delay = 0f;
+    public float delay = 2f;
+    private float timer = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
 
-        for (int i = 0; i < 5; i++)
-        {
-            var position = new Vector3(Random.Range(-3f, 3f), -3, 0);
-            TargetObj = Instantiate(TargetObj, position, Quaternion.identity);
-            Debug.Log("TargetObj created");
-
-        }
-        new WaitForSeconds(delay);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (timer < delay)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            SpawnTarget();
+            timer = 0;
+        }
+    }
 
+    void SpawnTarget()
+    {
+
+        var position = new Vector3(Random.Range(-3f, 3f), -3, 0);
+        Instantiate(TargetObj, position, Quaternion.identity);
     }
 }
